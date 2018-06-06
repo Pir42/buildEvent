d136f4e91150fd65490f0ebaeff479302370d59a
Pierre Moinet <pierre.moinet44600@gmail.com>
2018-06-06 12:29:32 +0200

    front create event

---
+++ app/assets/javascripts/profile.js
@@ -0,0 +1,5 @@
+document.getElementById('btnProfile').style.backgroundColor = "#0057ff";
+document.getElementById('profile').style.width = "130px";
+document.getElementById('textProfile').style.opacity = 1;
+document.getElementById('textProfile').style.transition = "none";
+document.getElementById('profile').style.transition = "none";
